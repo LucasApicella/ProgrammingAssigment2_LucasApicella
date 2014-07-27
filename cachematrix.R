8847af111d546127c929dcb4a5971761f37b8d65
LucasApicella <lucas.apicella@usp.br>
2014-07-27 14:06:39 -0300

    Commit #0002
    Making a few commets and making the final tests.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Objective: create two function to optimize the inverse matrix calculation
+
+## The first one, named 'makeCacheMatrix', stores the given matrix and return a list containing
+## The matrix itself, it's inverse matrix and two other functions to get those values.
+
+## The second function, named 'cacheSolve', does the inverse calculation. First, it searchs
+## for the inverse matrix. If the calculation were made before then the function doesn't
+## make the calculation but it returns the stored inverse matrix.
+## If the given matrix is a new one, then this function does the calculation and stores the
+## results for future reference.
 
 makeCacheMatrix <- function(x = matrix())
 {
@@ -9,17 +17,30 @@ makeCacheMatrix <- function(x = matrix())
 ## 3-) A function, named 'setInverseMatrix', which stores the inverse matrix value
 ## 4-) A function, named 'getInverseMatrix', which returns the matrix value
 
+  ## Starts the inverse matrix with NULL, because for new matrices (for example 'matrix')
+  ## the returned list will be like (matrix, get(), null, getinverse())
   inverse <- NULL
-
+  
+  ## Function that stores a new matrix, aka a matrix whose inverse matrix
+  ## has not been calculated yet
   setMatrix <- function(matrix)
   {
     x <<- matrix
     inverse <<- NULL
   }
+  
+  ## Function to return the matrix itself
   getMatrix <- function() x
+  
+  ## Function to store the inverse matrix
   setInverseMatrix <- function(inverseMatrix) inverse <<- inverseMatrix
+  
+  ## Function to return the inverse matrix that has been calculated before
   getInverseMatrix <- function() inverse
   
+  ## return the list containing the matrix, inverse matrix
+  ## (that will be NULL in case of a new matrix), and the two functions to get the matrix value
+  ## and the inverse matrix value
   list(setMatrix = setMatrix, getMatrix = getMatrix,
        setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
@@ -31,17 +52,24 @@ cacheSolve <- function(x, ...)
 ## inverse matrix calculated before
 ## In case the given matrix didn't have the inverse matrix stored
 ## then the 'solve' function will be called
+  
+  ## Get the inverse of the given matrix
+  inverse <- x$getInverseMatrix()
 
-inverse <- x$getInverseMatrix()
+  ## Testing the inverse matrix: in case the inverse were calculated before, return it
+  ## Otherwise, call the 'solve' functio to make the calculation
+  if(!is.null(inverse))
+  {
+    message("Getting Cached Matrix")
+    return(inverse)
+  }
 
-if(!is.null(inverse))
-{
-  message("Getting Cached Matrix")
-  return(inverse)
-}
-
-newMatrix <- x$getMatrix()
-inverse <- solve(newMatrix, ...)
-x$setInverseMatrix(inverse)
-inverse
+  ## Making the inverse calculation, and stroring the given matrix
+  ## and it's inverse for future reference
+  newMatrix <- x$getMatrix()
+  inverse <- solve(newMatrix, ...)
+  x$setInverseMatrix(inverse)
+  
+  ## Returnig the inverse matrix
+  inverse
 }
